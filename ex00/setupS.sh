4e7813a5e9060eb9fe020b829900fd93fce85e73
Fabio Carvalho Lopes <facarval@f2r5s10.clusters.42paris.fr>
2025-06-20 15:47:07 +0200

    ex00 Done

---
+++ ex00/setupS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install curl -y
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --node-external-ip=192.168.56.110 --flannel-iface=eth1" sh -s || echo "Failed to install K3s server"
+
+cp /var/lib/rancher/k3s/server/node-token /vagrant
