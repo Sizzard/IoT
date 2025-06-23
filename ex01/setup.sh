d598b055de76c765477797de3924a5ff8ad9f0f0
Fabio Carvalho Lopes <facarval@f2r5s10.clusters.42paris.fr>
2025-06-23 16:12:38 +0200

    ex02 Done

--- ex01/setup.sh
+++ ex01/setup.sh
@@ -4,3 +4,5 @@ sudo apt-get update -y
 sudo apt-get install curl -y
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --node-external-ip=192.168.56.110 --flannel-iface=eth1" sh -s || echo "Failed to install K3s server"
+
+sudo kubectl apply -f /vagrant/config
