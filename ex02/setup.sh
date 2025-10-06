65469a44d3ad1e7137412f0f75c49d41d7369c26
Fabio Carvalho Lopes <facarval@f1br4s8.clusters.42paris.fr>
2025-10-06 10:59:55 +0200

    Added app.yml

--- ex02/setup.sh
+++ ex02/setup.sh
@@ -24,13 +24,15 @@ alias k="kubectl"
 
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-k3d cluster create argocd
+k3d cluster create argocd --port "30088:30088@server:0"
 
 kubectl create namespace argocd
 kubectl create namespace dev
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443 # Launching server and expose port for accessing via website 
+kubectl apply -n argocd -f app.yml
+
+# Launching server and expose port for accessing via website              kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 # PASSWORD COMMAND :            kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > password.txt && echo "$(cat password.txt)"
