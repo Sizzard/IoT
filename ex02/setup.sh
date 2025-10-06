cf40f0d6656e19b850568ad4e648338c3991b698
Fabio Carvalho Lopes <facarval@f1br4s8.clusters.42paris.fr>
2025-10-06 10:22:02 +0200

    Changed deployment to wil docker image

--- ex02/setup.sh
+++ ex02/setup.sh
@@ -31,5 +31,6 @@ kubectl create namespace dev
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# PASSWORD COMMAND :            kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > password.txt && echo "$(cat password.txt)"
+kubectl port-forward svc/argocd-server -n argocd 8080:443 # Launching server and expose port for accessing via website 
 
+# PASSWORD COMMAND :            kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > password.txt && echo "$(cat password.txt)"
